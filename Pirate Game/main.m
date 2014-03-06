06a5950e0ef8b3f784ef606182cc1077a417a8fa
kenwilcox <wilcoxk@gmail.com>
2014-03-05 22:31:48 -0700

    Initial Commit

---
+++ Pirate Game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pirate Game
+//
+//  Created by Kenneth Wilcox on 3/5/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([KXAppDelegate class]));
+  }
+}
